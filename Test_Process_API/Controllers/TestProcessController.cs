a52537db591042e69de5db6972c02588c4ca2b13
Vlad <51176235+Vladoffz@users.noreply.github.com>
2021-06-08 19:02:38 +0300

    Update TestProcessController.cs

--- Test_Process_API/Controllers/TestProcessController.cs
+++ Test_Process_API/Controllers/TestProcessController.cs
@@ -55,6 +55,10 @@ namespace Test_Process_API.Controllers
         {
             return Ok();
         }
-
+        
+        [HttPut("Dungeon")]
+        public IActionResult Master(){
+        throw new Exception();
+        }
     }
 }
