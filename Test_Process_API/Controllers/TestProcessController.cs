23c2bf75c72179698570749f41f993f23f6cc65f
who am i <60845315+prof1A@users.noreply.github.com>
2021-06-08 19:30:11 +0300

    Update TestProcessController.cs

--- Test_Process_API/Controllers/TestProcessController.cs
+++ Test_Process_API/Controllers/TestProcessController.cs
@@ -56,7 +56,7 @@ namespace Test_Process_API.Controllers
             return Ok();
         }
         
-        [HttPut("Dungeon")]
+        [HttpPut("Dungeon")]
         public IActionResult Master(){
         throw new Exception();
         }
